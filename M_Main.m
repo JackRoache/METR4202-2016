a7f826ecc5f29d1ab260a7fb7092802ab21f403f
Jack <roachie@live.com.au>
2016-10-05 18:26:27 +1000

    Small fix to the Z coordinate. Forgot to rotate it to the reference frame.

--- M_Main.m
+++ M_Main.m
@@ -26,8 +26,10 @@ trigger(colorVid);
 stop(colorVid);
 
 %1000 is whats measured. 1250mm^2 for the domino. 
-distance = M_Distance(pixelArea, 1000, 1250) - FrameT(3);
+distance = M_Distance(pixelArea, 1000, 1250);
 [x,y] = cameraParams.pointsToWorld(FrameR, FrameT, undistortPoints(pixelPoint, cameraparams));
+z = Frame(t) - distance * FrameR;
+z = z(3);
 %x,y and z are wrt to the real world.
 
 
