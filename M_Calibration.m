4cbe0cab5ad853a6621674aa7e383362473125fd
Jack <roachie@live.com.au>
2016-10-06 16:31:31 +1000

    Calibration code touched up and working.  Beginning of the main function

--- M_Calibration.m
+++ M_Calibration.m
@@ -2,32 +2,53 @@
 
 %% Take photos
 function [params, estimateErrors, frames] = M_Calibration(colorVid)
-
+squareSize = 25.125;
 frames = [];
-NumberOfPhotos = 4;
+NumberOfPhotos = 2;
 
 start(colorVid);
-for k = 1:NumberOfPhotos
-    display(sprintf('Ready to take Frame %d/%d. Press any key to take next frame...', k, NumberOfPhotos));
+sumErrors = 100;
+
+display(sprintf('Ready to take Frame 1. Press any key to take next frame...'));
+pause
+trigger(colorVid);
+[f, ~, ~] = getdata(colorVid);
+
+%Flips left and right to give real view with fliplr
+frames = cat(4,frames, fliplr(f));
+
+while sumErrors > 5;
+    display(sprintf('Ready to take Frame %d. Press any key to take next frame...',NumberOfPhotos));
     pause
     trigger(colorVid);
-    [f, time, meta] = getdata(colorVid);
+    [f, ~, ~] = getdata(colorVid);
     
     %Flips left and right to give real view with fliplr
     frames = cat(4,frames, fliplr(f));
+    try
+        [imagePoints, boardSize, ~] = detectCheckerboardPoints(frames);
+        
+        worldPoints = generateCheckerboardPoints(boardSize, squareSize);
+        
+        [params, used, estimateErrors] = estimateCameraParameters(imagePoints, worldPoints);
+        
+        sumErrors = mean(estimateErrors.ExtrinsicsErrors.TranslationVectorsError);
+        if used(end) > 0
+            NumberOfPhotos = NumberOfPhotos + 1;
+            displayErrors(estimateErrors, params);
+        else
+            display('Poor image taken, new frame needed');
+        end
+        
+    catch ME
+        display('Poor image taken, new frame needed');
+    end
 end
 stop([colorVid]);
 
-display('All Frames Captured. Calibrating...');
-
-[imagePoints, boardSize, imagesUsed] = detectCheckerboardPoints(frames);
-
-squareSize = 25.125;
-worldPoints = generateCheckerboardPoints(boardSize, squareSize);
-
-[params, ~, estimateErrors] = estimateCameraParameters(imagePoints, worldPoints);
-
 % figure(1);
-% showExtrinsics(params, 'CameraCentric');
-% displayErrors(estimateErrors, params);
+showExtrinsics(params, 'CameraCentric');
+displayErrors(estimateErrors, params);
+display(sprintf('\n\n\nNumber of frames used: %d\n\n\n', sum(used)));
+
 end
--- M_Main.m
+++ M_Main.m
@@ -1,35 +1,64 @@
-%%
-% clear
-% clc
+try 
+    stop(colorVid);
+catch ME
+end
 
-%%
-% [colorVid, depthVid] = M_StartKinect();
+[colorVid, depthVid] = M_StartKinect();
 
 % Basic testing code. Takes in the webcam input.
-delete colorVid;
-clear colorVid;
-colorVid = videoinput('winvideo', 1);
-colorVid.FramesPerTrigger = 1;
-colorVid.TriggerRepeat = Inf;
-triggerconfig(colorVid, 'manual');
+% delete colorVid;
+% clear colorVid;
+% colorVid = videoinput('winvideo', 1);
+% colorVid.FramesPerTrigger = 1;
+% colorVid.TriggerRepeat = Inf;
+% triggerconfig(colorVid, 'manual');
 
 %%
 [cameraParams, estimateError, frames] = M_Calibration(colorVid);
 
 %%
-[FrameT,FrameR] = M_SensorPlacement(colorVid, frames);
+[FrameT,FrameR] = M_SensorPlacement(colorVid, frames, cameraParams);
 
-pause;
 start(colorVid);
-trigger(colorVid);
-[f, time, meta] = getdata(colorVid);
-stop(colorVid);
 
-%1000 is whats measured. 1250mm^2 for the domino. 
-distance = M_Distance(pixelArea, 1000, 1250);
-[x,y] = cameraParams.pointsToWorld(FrameR, FrameT, undistortPoints(pixelPoint, cameraparams));
-z = Frame(t) - distance * FrameR;
-z = z(3);
+% clf
+
+while 1
+    % Take Frame
+    trigger(colorVid);
+    [colorIm, time, meta] = getdata(colorVid);
+    % Image Filter
+    
+    % Blob Analysis
+    
+     % Hough Lines Analysis
+    
+    % Object Filter
+    
+    % Positioning
+    
+    % Tracking
+    
+    % Display Data
+    
+    hold on
+%     imshow(colorIm);
+    
+    hold off;
+end
+
+
+
+% pause;
+% start(colorVid);
+% trigger(colorVid);
+% [f, time, meta] = getdata(colorVid);
+% stop(colorVid);
+% %1000 is whats measured. 1250mm^2 for the domino. 
+% distance = M_Distance(ixelArea, 1000, 1250);
+% [x,y] = cameraParams.pointsToWorld(FrameR, FrameT, undistortPoints(pixelPoint, cameraparams));
+% z = Frame(t) - distance * FrameR;
+% z = z(3);
 %x,y and z are wrt to the real world.
 
 
